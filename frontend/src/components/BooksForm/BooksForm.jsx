52236b4576dfac32e3051b2b293e064861c4822c
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-06-03 10:00:59 +0300

    We made styles for Bookworm and linked states to inputs

--- frontend/src/components/BooksForm/BooksForm.jsx
+++ frontend/src/components/BooksForm/BooksForm.jsx
@@ -1,8 +1,40 @@
+import { useState } from 'react';
 import styles from './BooksForm.module.scss';
 const BooksForm = () => {
+  // Состояние поля title
+  const [title, setTitle] = useState('');
+  // Состояние поля author
+  const [author, setAuthor] = useState('');
+  const onSubmitHandler = (e) => {
+    // Чтобы страница не перезагружалась
+    e.preventDefault();
+  };
   return (
-    <div className={styles.appBlock}>
+    <div className={` ${styles.appBlock} ${styles.bookForm}`}>
       <h1>Add a new Book</h1>
+      <form onSubmit={onSubmitHandler} className={styles}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            placeholder="Enter the title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author</label>
+          <input
+            type="text"
+            placeholder="Enter the author"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+        <button>Add Book</button>
+      </form>
     </div>
   );
 };
