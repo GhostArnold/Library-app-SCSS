dbae1653d39de2ffcedde3bdb87583297db2d234
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-06-05 15:00:20 +0300

    Implemented the functionality of displaying the status of the redux in the BooksList

--- frontend/src/components/BooksList/BooksList.jsx
+++ frontend/src/components/BooksList/BooksList.jsx
@@ -1,8 +1,28 @@
+import { useSelector } from 'react-redux';
 import styles from './BooksList.module.scss';
 const BooksList = () => {
+  //  это хук, предоставляемый библиотекой react-redux, который позволяет компонентам React получать данные из хранилища Redux.
+  // books - название редьюсера
+  const books = useSelector((state) => state.books);
   return (
     <div className={styles.appBlock}>
       <h1>Books list: </h1>
+      {/* Делаем проверку. Если длинна массива равна ноль, то отображаем  "Not books avaliable", иначе выводим список книг с помощью map*/}
+      {books.length === 0 ? (
+        <h3>Not books avaliable</h3>
+      ) : (
+        books.map((book, i) => {
+          return (
+            <ul>
+              <li key={i}>
+                <div>
+                  {++i}. {book.title} by {book.author}
+                </div>
+              </li>
+            </ul>
+          );
+        })
+      )}
     </div>
   );
 };
