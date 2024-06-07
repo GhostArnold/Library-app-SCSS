50bc59ee91fad4ae811439597bd0efabe3df7921
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-06-07 10:17:41 +0300

    Added input to Filter.jsx

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -3,6 +3,9 @@ const Filter = () => {
   return (
     <div className={styles.appBlock}>
       <h1>Filter</h1>
+      <div>
+        <input type="text" placeholder="Filter by title" />
+      </div>
     </div>
   );
 };
