00fbd56503d0df202766be62dbd457ec98d887e5
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-06-12 15:20:16 +0300

    I wrote comments for a cleaner perception

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,22 +1,29 @@
 import { useSelector, useDispatch } from 'react-redux';
+//  Импортируем селекторы состояния из файла filterSlice для получения текущих фильтров
 import {
   selectTitleFilter,
   selectAuthorFilter,
 } from '../../redux/slices/filterSlice';
+// Импортируем экшены для изменения состояния фильтров
 import {
   setTitleFilter,
-  setAuthorFiltter,
+  setAuthorFilter,
 } from '../../redux/slices/filterSlice';
 import styles from './Filter.module.scss';
 const Filter = () => {
   const dispatch = useDispatch();
+  // Получаем текущие значения фильтров используя селекторы и хук useSelector
   const titleFilter = useSelector(selectTitleFilter);
   const authorFilter = useSelector(selectAuthorFilter);
+
+  // Обработчик изменения поля фильтра по названию
   const handleTitleChange = (e) => {
     dispatch(setTitleFilter(e.target.value));
   };
+
+  // Обработчик изменения поля фильтра по автору
   const handleAuthorChange = (e) => {
-    dispatch(setAuthorFiltter(e.target.value));
+    dispatch(setAuthorFilter(e.target.value));
   };
   return (
     <div className={styles.appBlock}>
--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -1,22 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Начальное состояние фильтров
 const initialState = {
   title: '',
   author: '',
 };
+// Создаем slice (порцию состояния) для фильтров
 const filterSlice = createSlice({
   name: 'filter',
   initialState,
   reducers: {
+    // Редьюсер для установки фильтра по заголовку
     setTitleFilter: (state, action) => {
       state.title = action.payload;
     },
+    // Редьюсер для установки фильтра по автору
     setAuthorFiltter: (state, action) => {
       state.author = action.payload;
     },
   },
 });
-export const { setTitleFilter, setAuthorFiltter } = filterSlice.actions;
+// Экспорт экшенов
+export const { setTitleFilter, setAuthorFilter } = filterSlice.actions;
+// Селекторы для получения значений фильтров из состояния
 export const selectTitleFilter = (state) => state.filter.title;
 export const selectAuthorFilter = (state) => state.filter.author;
 export default filterSlice.reducer;
