dd6009eeac5ecf9eb2d093deb4d63e73189e095e
Artem Prudnikov <artemprudnikov907@gmail.com>
2024-06-12 15:00:27 +0300

    I made a synchronized redux state for the author input

--- frontend/src/components/Filter/Filter.jsx
+++ frontend/src/components/Filter/Filter.jsx
@@ -1,12 +1,22 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { selectTitleFilter } from '../../redux/slices/filterSlice';
-import { setTitleFilter } from '../../redux/slices/filterSlice';
+import {
+  selectTitleFilter,
+  selectAuthorFilter,
+} from '../../redux/slices/filterSlice';
+import {
+  setTitleFilter,
+  setAuthorFiltter,
+} from '../../redux/slices/filterSlice';
 import styles from './Filter.module.scss';
 const Filter = () => {
   const dispatch = useDispatch();
   const titleFilter = useSelector(selectTitleFilter);
+  const authorFilter = useSelector(selectAuthorFilter);
   const handleTitleChange = (e) => {
     dispatch(setTitleFilter(e.target.value));
+  };
+  const handleAuthorChange = (e) => {
+    dispatch(setAuthorFiltter(e.target.value));
   };
   return (
     <div className={styles.appBlock}>
@@ -18,7 +28,12 @@ const Filter = () => {
           value={titleFilter}
           onChange={handleTitleChange}
         />
-        <input type="text" placeholder="Filter by author" />
+        <input
+          type="text"
+          placeholder="Filter by author"
+          value={authorFilter}
+          onChange={handleAuthorChange}
+        />
       </div>
     </div>
   );
--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   title: '',
+  author: '',
 };
 const filterSlice = createSlice({
   name: 'filter',
@@ -10,8 +11,12 @@ const filterSlice = createSlice({
     setTitleFilter: (state, action) => {
       state.title = action.payload;
     },
+    setAuthorFiltter: (state, action) => {
+      state.author = action.payload;
+    },
   },
 });
-export const { setTitleFilter } = filterSlice.actions;
+export const { setTitleFilter, setAuthorFiltter } = filterSlice.actions;
 export const selectTitleFilter = (state) => state.filter.title;
+export const selectAuthorFilter = (state) => state.filter.author;
 export default filterSlice.reducer;
